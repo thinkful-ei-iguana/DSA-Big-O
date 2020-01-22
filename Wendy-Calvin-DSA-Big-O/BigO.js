5d665289ceec1db4481f7d4efc5fe69086f8ae9c
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-01-22 13:01:25 -0800

    drills

--- Wendy-Calvin-DSA-Big-O/BigO.js
+++ Wendy-Calvin-DSA-Big-O/BigO.js
@@ -1,16 +1,44 @@
-function TOH(n, source, dest, via) {
-  if (n >= 1) {
-    // Move a tower of n-1 to the buffer peg, using the destination peg.
-    TOH(n - 1, source, via, dest);
+// function TOH(n, source, dest, via) {
+//   if (n >= 1) {
+//     // Move a tower of n-1 to the buffer peg, using the destination peg.
+//     TOH(n - 1, source, via, dest);
 
-    // Move the remaining disk to the destination peg.
-    console.log("Move disk from Tower ", source, " to Tower ", dest);
+//     // Move the remaining disk to the destination peg.
+//     console.log("Move disk from Tower ", source, " to Tower ", dest);
 
-    // Move the tower of `n-1` from the `buffer peg` to the `destination peg` using the `source peg`.
-    TOH(n - 1, via, dest, source);
+//     // Move the tower of `n-1` from the `buffer peg` to the `destination peg` using the `source peg`.
+//     TOH(n - 1, via, dest, source);
+//   }
+
+//   return;
+// }
+
+// TOH(3, "A", "C", "B");
+
+//Drill 1 iterative, counting sheep
+
+function sheepCounter(n) {
+  for (let i = n; n >= 0; i--) {
+    if (i > 0) {
+      console.log(`${i}: Another sheep jumps over the fence`);
+    } else {
+      console.log("All sheep jumped over the fence.");
+      return;
+    }
   }
-
-  return;
 }
 
-TOH(3, "A", "C", "B");
+sheepCounter(3);
+
+function powerCalc(base, exp) {
+  return Math.pow(base, exp);
+}
+
+function reverseString(str) {
+  let newStr = "";
+  for (let i = 0; i < str.length; i++ ) {
+    if (str.charAt(str.length -1) ) {
+      newStr = newStr + 
+    }
+  }
+}
